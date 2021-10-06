5f7ccc8f6a8b113f35545f073531753d31de683c
Omar Hernandez <omar.hernandez@acoustic.co>
2021-10-05 17:51:57 -0700

    EAOC-4945: Refactor and moved to repo location.

---
+++ ModalView/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  SwiftUIMindBlowing
+//
+//  Created by ohernandezltkmac on 10/5/21.
+//  Copyright © 2021 An Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
